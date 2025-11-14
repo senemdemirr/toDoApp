dc7b5426de86f12e9aebb12a4a809041d9c862c2
senemdemirr <senemdemir262@gmail.com>
2025-11-14 22:06:40 +0300

    completed app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,21 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
-<body class="bg-ligth">
-    
+<body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="{{route ('tasks.index') }}" class="navbar-brand">To Do App</a>
+        </div>
+    </nav>
+    <div class="container py-4">
+        @if(session('success'))
+            <div class="alert alert-success">
+               {{ session('success')}}
+            </div>
+        @endif
+
+        @yield('content')
+    </div>
 </body>
 
 </html>
